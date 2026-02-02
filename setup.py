2175f1a79b59695aa66c629414017fbf1e04ad60
Victor <victor.soares@live.it>
2026-02-02 12:45:12 -0300

    Bump version to 1.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ai-screenshooter",
-    version="1.6.0",
+    version="1.7.0",
     packages=find_packages(),
     py_modules=["ai_screenshot"],
     install_requires=[
